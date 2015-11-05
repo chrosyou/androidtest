18b539c45405b79be76b58fcc05d74ba1a656788
chrosyou <646680299@qq.com>
2015-11-05 16:43:37 +0800

    callback

---
+++ check/callbackbuton/app/src/main/java/com/macalyou/murisly/callbackbuton/myclass.java
@@ -0,0 +1,31 @@
+package com.macalyou.murisly.callbackbuton;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2015/11/5.
+ */
+public class myclass extends Button {
+    public myclass (Context context, AttributeSet set)
+    {
+        super(context, set);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        super.onKeyDown(keyCode, event);
+        Log.v("-crazyit.org-", "the key down");
+
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaa");
+        TextView show = (TextView)findViewById(R.id.tvshow);
+        show.setText("已经点击");
+        //返回true表示事件不会扩散
+        return true;
+    }
+}
