6eeb710f806fcd16d29a9696baf27c86c3c1755d
chrosyou <646680299@qq.com>
2015-11-26 00:32:22 +0800

    view animation

---
+++ androidhero/MyApplication2/app/src/main/java/com/example/macalyou/viewanimation/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.macalyou.viewanimation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void btnAlpha(View view) {
+        AlphaAnimation aa = new AlphaAnimation(0, 1);
+        aa.setDuration(1000);
+        view.startAnimation(aa);
+    }
+
+    public void btnRotate(View view) {
+        RotateAnimation ra = new RotateAnimation(0, 360, 100, 100);
+        ra.setDuration(1000);
+        view.startAnimation(ra);
+    }
+
+    public void btnRotateSelf(View view) {
+        RotateAnimation ra = new RotateAnimation(0, 360,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5F,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5F);
+        ra.setDuration(1000);
+        view.startAnimation(ra);
+    }
+
+    public void btnTranslate(View view) {
+        TranslateAnimation ta = new TranslateAnimation(0, 200, 0, 300);
+        ta.setDuration(1000);
+        view.startAnimation(ta);
+    }
+
+    public void btnScale(View view) {
+        ScaleAnimation sa = new ScaleAnimation(0, 2, 0, 2);
+        sa.setDuration(1000);
+        view.startAnimation(sa);
+    }
+
+    public void btnScaleSelf(View view) {
+        ScaleAnimation sa = new ScaleAnimation(0, 1, 0, 1,
+                Animation.RELATIVE_TO_SELF, 0.5F,
+                Animation.RELATIVE_TO_SELF, 0.5F);
+        sa.setDuration(1000);
+        view.startAnimation(sa);
+    }
+
+    public void btnSet(View view) {
+        AnimationSet as = new AnimationSet(true);
+        as.setDuration(1000);
+
+        AlphaAnimation aa = new AlphaAnimation(0, 1);
+        aa.setDuration(1000);
+        as.addAnimation(aa);
+
+        TranslateAnimation ta = new TranslateAnimation(0, 100, 0, 200);
+        ta.setDuration(1000);
+        as.addAnimation(ta);
+
+        view.startAnimation(as);
+    }
+}
